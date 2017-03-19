8cf067c5197cdee2d6b12c62e18323d3a5909680
Blake Knight <oss.ideas@gmail.com>
2017-03-18 21:40:12 -0500

    :white_check_mark: test object/array export expected format

--- __tests__/array.js
+++ __tests__/array.js
@@ -5,3 +5,13 @@ import zipCount from '../zipCount'
 test('exports the correct number of zips', t => {
   t.is(zipArray.length, zipCount)
 })
+
+test('exports the expected format', t => {
+  t.true(Array.isArray(zipArray))
+
+  t.is(typeof zipArray[0], 'object')
+
+  t.truthy(zipArray[0].latitude)
+  t.truthy(zipArray[0].longitude)
+  t.truthy(zipArray[0].zipCode)
+})
--- __tests__/object.js
+++ __tests__/object.js
@@ -6,3 +6,13 @@ test('exports the correct number of zips', t => {
   const numberExported = Object.keys(usZips).length
   t.is(numberExported, zipCount)
 })
+
+test('exports the expected format', t => {
+  t.is(typeof usZips, 'object')
+
+  const testZip = usZips['37214']
+
+  t.is(typeof testZip, 'object')
+  t.truthy(testZip.latitude)
+  t.truthy(testZip.longitude)
+})
