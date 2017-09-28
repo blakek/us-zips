715b13529cb22e49a0b1945482ad14f34c531ed2
Blake Knight <oss.ideas@gmail.com>
2017-09-27 21:17:54 -0500

    :truck: flatten file structure

--- scripts/make-array.js
+++ scripts/make-array.js
@@ -3,18 +3,13 @@ const path = require('path')
 const util = require('util')
 const usZips = require('..')
 
-const outputFile = path.join(__dirname, '../array/index.js')
+const outputFile = path.join(__dirname, '../array.js')
 
 const outputStart = 'module.exports = [\n  '
 const outputEnd = '\n]\n'
 
-const outputBody = Object
-  .keys(usZips)
+const outputBody = Object.keys(usZips)
   .map(zipCode => JSON.stringify(Object.assign(usZips[zipCode], { zipCode })))
   .join(',\n  ')
 
-fs.writeFileSync(
-  outputFile,
-  outputStart + outputBody + outputEnd,
-  'utf8'
-)
+fs.writeFileSync(outputFile, outputStart + outputBody + outputEnd, 'utf8')
--- scripts/make-map.js
+++ scripts/make-map.js
@@ -3,18 +3,13 @@ const path = require('path')
 const util = require('util')
 const usZips = require('..')
 
-const outputFile = path.join(__dirname, '../map/index.js')
+const outputFile = path.join(__dirname, '../map.js')
 
 const outputStart = 'module.exports = new Map([\n  '
 const outputEnd = '\n])\n'
 
-const outputBody = Object
-  .keys(usZips)
+const outputBody = Object.keys(usZips)
   .map(zipCode => JSON.stringify([zipCode, usZips[zipCode]]))
   .join(',\n  ')
 
-fs.writeFileSync(
-  outputFile,
-  outputStart + outputBody + outputEnd,
-  'utf8'
-)
+fs.writeFileSync(outputFile, outputStart + outputBody + outputEnd, 'utf8')
